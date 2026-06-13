9700240bed141c44645a2231bff59b3a6b9a2bfc
taufikapt1983-prog <taufik.apt.1983@gmail.com>
2026-06-13 10:24:42 +0700

    Create tambah-kk.js

---
+++ js/tambah-kk.js
@@ -0,0 +1,34 @@
+const API_URL = "URL_WEBAPP_ANDA";
+
+async function simpanKK(){
+
+    const payload = {
+
+        no_kk : document.getElementById("no_kk").value,
+        kepala_keluarga : document.getElementById("kepala_keluarga").value,
+        alamat : document.getElementById("alamat").value,
+        rt : document.getElementById("rt").value,
+        rw : document.getElementById("rw").value,
+        dusun : document.getElementById("dusun").value,
+        desa : document.getElementById("desa").value,
+        kecamatan : document.getElementById("kecamatan").value,
+        kabupaten : document.getElementById("kabupaten").value,
+        jumlah_anggota : 0,
+        no_hp : document.getElementById("no_hp").value
+
+    };
+
+    const response = await fetch(
+        API_URL + "?action=tambahKK",
+        {
+            method:"POST",
+            body:JSON.stringify(payload)
+        }
+    );
+
+    const data = await response.json();
+
+    document.getElementById("hasil").innerHTML =
+    data.pesan;
+
+}
