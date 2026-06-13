46e6a97635cf881bd5ef5b581747ec6091d50839
taufikapt1983-prog <taufik.apt.1983@gmail.com>
2026-06-13 09:52:59 +0700

    Create tambah-penduduk.js

---
+++ js/tambah-penduduk.js
@@ -0,0 +1,60 @@
+const API_URL =
+"https://script.google.com/macros/s/AKfycbx_7IcjH61WK90ffxDRmrLA0YUGVy2Ra6ioTY4FK7MpDUS7odGcRFIH-8QRz4Bdk-Rf7w/exec";
+
+async function simpanPenduduk(){
+
+    const payload = {
+
+        nik :
+        document.getElementById("nik").value,
+
+        no_kk :
+        document.getElementById("no_kk").value,
+
+        nama :
+        document.getElementById("nama").value,
+
+        jk :
+        document.getElementById("jk").value,
+
+        tempat_lahir:"",
+        tanggal_lahir:"",
+        agama:"",
+        pendidikan:"",
+        pekerjaan:"",
+        status_kawin:"",
+        status_hubungan:"",
+        kewarganegaraan:"WNI",
+        alamat:"",
+        rt:"",
+        rw:"",
+        dusun:"",
+        desa:"",
+        kecamatan:"",
+        kabupaten:"",
+        no_hp:"",
+        bpjs:"",
+        koordinat_rumah:""
+
+    };
+
+    const response = await fetch(
+
+        API_URL +
+        "?action=tambahPenduduk",
+
+        {
+            method:"POST",
+            body:JSON.stringify(payload)
+        }
+
+    );
+
+    const data =
+    await response.json();
+
+    document.getElementById("hasil")
+    .innerHTML =
+    data.pesan;
+
+}
